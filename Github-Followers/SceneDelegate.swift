299937b513bb23b8d08878e9fdf945796b2c178c
Petre Vane <PetreVane@users.noreply.github.com>
2020-01-02 14:59:44 +0100

    Added assets; added root ViewController & added a test background color

--- Github-Followers/SceneDelegate.swift
+++ Github-Followers/SceneDelegate.swift
@@ -14,10 +14,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        // init window with the frameSize of the windowScene bounds
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        //assign windowScene to window
+        window?.windowScene = windowScene
+        //assign the root ViewController
+        window?.rootViewController = ViewController()
+        // show the ViewController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
