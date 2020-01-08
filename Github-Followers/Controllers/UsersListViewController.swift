3ab7ccfead206823bacbaf8247bbcdb4c81d7bb1
Petre Vane <PetreVane@users.noreply.github.com>
2020-01-08 21:12:56 +0100

    Added new Support grouping

--- Github-Followers/Controllers/UsersListViewController.swift
+++ Github-Followers/Controllers/UsersListViewController.swift
@@ -13,16 +13,19 @@ class UsersListViewController: UIViewController {
  //MARK: - Initialization
     
     var user: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
         print("User initilized with value: \(user)")
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
-
-
+    
+    override func viewWillAppear( _ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = false
+    }
 }
