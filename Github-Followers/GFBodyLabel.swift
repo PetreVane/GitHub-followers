2f17b25e92e09b5fdfc6f2b8c33c07ebfc25ee82
Petre Vane <PetreVane@users.noreply.github.com>
2020-01-08 12:26:46 +0100

    Added 2 new label models

---
+++ Github-Followers/GFBodyLabel.swift
@@ -0,0 +1,40 @@
+//
+//  GFBodyLabel.swift
+//  Github-Followers
+//
+//  Created by Petre Vane on 08/01/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        //autoLayout
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        // text attributes
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+    }
+
+}
---
+++ Github-Followers/GFTitleLabel.swift
@@ -0,0 +1,47 @@
+//
+//  GFTitleLabel.swift
+//  Github-Followers
+//
+//  Created by Petre Vane on 08/01/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    
+    private func configure() {
+        // autoLayout
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        // text attributes
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        numberOfLines = 1
+        
+        // visual attributes
+        layer.cornerRadius = 10
+        backgroundColor = UIColor.systemBackground
+        
+    }
+}
+
