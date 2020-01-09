ef459de3b92385f41d7fc5e242958196af335d35
Petre Vane <PetreVane@users.noreply.github.com>
2020-01-09 18:00:02 +0100

    Added an Error Manager which enumerates the possible case errors.

---
+++ Github-Followers/Managers/ErrorManager.swift
@@ -0,0 +1,36 @@
+//
+//  ErrorManager.swift
+//  Github-Followers
+//
+//  Created by Petre Vane on 09/01/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import Foundation
+
+
+enum ErrorManager: Error {
+    case failedURL
+    case failedNetworkRequest
+    case unexpectedStatusCode
+    case invalidData
+    case failedJSONParsing
+    
+    
+    var localizedDescription: String {
+        
+        switch self {
+            
+        case .failedURL:
+            return "Soometing went wrong when trying to cast String type to URL type"
+        case .failedNetworkRequest:
+            return "Something went wrong. Make sure you're connected to Internet and try again later"
+        case .unexpectedStatusCode:
+            return "Server responded with unexpected status code."
+        case .invalidData:
+            return "Invalid data returned by the server."
+        case .failedJSONParsing:
+            return "Failed parsing JSON returned by your network request"
+        }
+    }
+}
