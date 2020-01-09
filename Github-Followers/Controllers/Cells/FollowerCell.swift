e21fbccf5263c48772b67c0308ee0b9f3d1695ab
Petre Vane <PetreVane@users.noreply.github.com>
2020-01-09 21:46:26 +0100

    Added collectionView cell model + custom imageView model

---
+++ Github-Followers/Controllers/Cells/FollowerCell.swift
@@ -0,0 +1,41 @@
+//
+//  FollowerCell.swift
+//  Github-Followers
+//
+//  Created by Petre Vane on 09/01/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "followerCell"
+    let userNameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    let cellImage = GFAvatarImageView(frame: .zero)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        self.addSubview(cellImage)
+        self.addSubview(userNameLabel)
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            cellImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            cellImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            cellImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            cellImage.heightAnchor.constraint(equalTo: cellImage.widthAnchor)
+        ])
+    }
+}
---
+++ Github-Followers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,29 @@
+//
+//  GFAvatarImageView.swift
+//  Github-Followers
+//
+//  Created by Petre Vane on 09/01/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = UIImage(named: "avatar-placeholder")
+    }
+    
+}
