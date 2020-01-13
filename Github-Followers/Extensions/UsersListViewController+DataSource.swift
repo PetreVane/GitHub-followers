5346855652fb6bc971a741af964b7c8bdb2fb5a2
Petre Vane <PetreVane@users.noreply.github.com>
2020-01-13 21:56:08 +0100

    Added traditional DataSource to CollectionView

---
+++ Github-Followers/Extensions/UsersListViewController+DataSource.swift
@@ -0,0 +1,28 @@
+//
+//  UsersListViewController+DataSource.swift
+//  Github-Followers
+//
+//  Created by Petre Vane on 13/01/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import UIKit
+
+extension UsersListViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return listOfUsers.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FollowerCell.reuseIdentifier, for: indexPath) as? FollowerCell else { return UICollectionViewCell() }
+
+        cell.userNameLabel.text = listOfUsers[indexPath.row].login
+        return cell
+    }
+
+
+}
+
+extension UsersListViewController: UICollectionViewDelegate { }
+
