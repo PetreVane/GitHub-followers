9d95d5073631f43d16cd037e7dac000b64da15c6
Petre Vane <PetreVane@users.noreply.github.com>
2020-01-29 16:49:32 +0100

    Added multiple custom views to RCardController. Added views to stack and established how visual elements are presented

--- Github-Followers/Controllers/ReusableCardController.swift
+++ Github-Followers/Controllers/ReusableCardController.swift
@@ -9,22 +9,67 @@
 import UIKit
 
 class ReusableCardController: UIViewController {
+    
+    let stackView = UIStackView()
+    let firstView = ReusableView()
+    let secondView = ReusableView()
+    let actionButton = CustomButton()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        configureCardView()
+        configureCustomViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /// Sets ReusableCardController object visual properties
+    ///
+    /// Sets the background color and the corner radius
+    private func configureCardView() {
+        view.backgroundColor = .secondarySystemBackground
+        view.layer.cornerRadius = 12.5
     }
-    */
+    
+    /// Determines what stackView contains and how is presented
+    private func configureStackView() {
+        
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually // try .equalSpacing
+        
+        stackView.addArrangedSubview(firstView)
+        stackView.addArrangedSubview(secondView)
+        
+    }
+    
+    /// Sets constraints and visual properties of custom Views
+    ///
+    /// Adds views to stackView and determines how views are presented
+    private func configureCustomViews() {
+        
+        let padding: CGFloat = 20
+        let listOfViews = [stackView, actionButton]
+        
+        listOfViews.forEach { customView in
+            view.addSubview(customView)
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+        
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+            
+        ])
+    }
 
+   
 }
