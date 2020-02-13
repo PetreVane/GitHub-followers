ab5531a155546232538a1632563c1dd96877bbd0
Petre Vane <PetreVane@users.noreply.github.com>
2020-02-13 19:47:21 +0100

    Renaming file failed. TabBarController re-added

--- Github-Followers/Coordinators/MainTabBar.swift
+++ Github-Followers/Coordinators/MainTabBar.swift
@@ -1,10 +1,11 @@
 //
-//  TabBarController.swift
+//  MainTabBar.swift
 //  Github-Followers
 //
 //  Created by Petre Vane on 13/02/2020.
 //  Copyright © 2020 Petre Vane. All rights reserved.
 //
+
 
 import UIKit
 
