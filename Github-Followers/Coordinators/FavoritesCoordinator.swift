8ad4ad9bcce08d10ad75f7d8800f2fc90e59abf9
Petre Vane <PetreVane@users.noreply.github.com>
2020-02-14 20:06:51 +0100

    Deleted unused code

--- Github-Followers/Coordinators/FavoritesCoordinator.swift
+++ Github-Followers/Coordinators/FavoritesCoordinator.swift
@@ -8,29 +8,4 @@
 
 import UIKit
 
-/// Coordinates FavoritesController
-///
-/// Creates an object of FavoritesController and assigns it to a NavigationController object
-class FavoritesCoordinator: Coordinator {
-    
-    weak var coordinator: MainCoordinator?
-    var childCoordinators = [Coordinator]()
-    var navigationController: UINavigationController
-    
-    init(navigationController: UINavigationController) {
-        self.navigationController = navigationController
-    }
-    
-    /// Creates an object of FavoritesController
-    ///
-    /// This method creates an object of FavoritesController and assigns self as childCoordinator. Then, embeds FacoritesController into navigationController passed at class initialization.
-    func startFavoritesController() -> UINavigationController {
-        
-        let favController = FavoritesController()
-        favController.childCoordinator = self
-        favController.title = "Favorites"
-        favController.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-        navigationController.viewControllers = [favController]
-        return navigationController
-    }
-}
+
