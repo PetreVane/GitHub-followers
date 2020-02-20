e456de52a21e3146c1e13dc0624357f50d21198d
Petre Vane <PetreVane@users.noreply.github.com>
2020-02-20 14:34:32 +0100

    Added Router & Navigation router

---
+++ Github-Followers/Routers/NavigationRouter.swift
@@ -0,0 +1,105 @@
+//
+//  NavigationRouter.swift
+//  Github-Followers
+//
+//  Created by Petre Vane on 19/02/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import UIKit
+
+class NavigationRouter: NSObject {
+    
+    // parent ViewController that will instantiate this class
+    unowned let parentViewController: UIViewController
+    let navigationController = UINavigationController()
+    private var onDismissForViewController: [UIViewController: (() -> Void)] = [:]
+    
+    init(parentViewController: UIViewController) {
+        self.parentViewController = parentViewController
+        super.init()
+        // sets this class as delegate for UINavigationControllerDelegate; see Extensions
+        navigationController.delegate = self
+    }
+}
+
+extension NavigationRouter: Router {
+    
+    func present(_ viewController: UIViewController, animated: Bool, onDismiss: (() -> Void)?) {
+        
+        // associate the passed in closure with the passed in ViewController
+        onDismissForViewController[viewController] = onDismiss
+        
+        if navigationController.viewControllers.count == 0 {
+            
+            presentModally(viewController, animated: animated)
+        } else {
+            // push the viewContolle onto the navigationController stack
+            navigationController.pushViewController(viewController, animated: animated)
+        }
+    }
+    
+    func dismiss(animated: Bool) {
+        performOnDismissAction(for: navigationController.viewControllers.first!)
+        parentViewController.dismiss(animated: animated, completion: nil)
+    }
+    
+    // MARK: - Private methods
+    
+    /// Presents a viewController modally
+    /// - Parameters:
+    ///   - viewController: ViewController to be presented
+    ///   - animated: pass in true to animate the transition
+    /// - adds a button (calls a method to add a button)
+    /// - sets the viewController into navigationController's list of viewControllers
+    /// - calls the present method of the parentVC and passes in the navigationController containg the ViewController
+    private func presentModally( _ viewController: UIViewController, animated: Bool) {
+        
+        addCancelButton(to: viewController)
+        navigationController.setViewControllers([viewController], animated: animated)
+        parentViewController.present(navigationController, animated: animated, completion: nil)
+    }
+    
+    /// Adds a navigation bar button item
+    /// - Parameter viewController: viewController that contains the button
+    private func addCancelButton(to viewController: UIViewController) {
+        viewController.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(cancelButtonPressed))
+    }
+    
+    /// Triggers an action when a button is pressed
+    @objc private func cancelButtonPressed() {
+        performOnDismissAction(for: navigationController.viewControllers.first!)
+        parentViewController.dismiss(animated: true, completion: nil)
+    }
+    
+    /// Executes a closure when ViewController is dismissed
+    /// - Parameter viewController: ViewController which is being dismissed
+    /// - makes sure there is an associated closure for the passed in argument
+    /// - executes the closure
+    /// - removes the closure from dictionary
+    private func performOnDismissAction(for viewController: UIViewController) {
+        
+        guard let onDismissAction = onDismissForViewController[viewController] else { return }
+        onDismissAction()
+        onDismissForViewController.removeValue(forKey: viewController)
+    }
+}
+
+extension NavigationRouter: UINavigationControllerDelegate {
+    
+    
+    /// Dismisses a viewController when the NavigationController back button is pressed
+    /// - Parameters:
+    ///   - navigationController: navigationController containing the ViewController that is being presented
+    ///   - viewController: ViewController that is about to be dimissed
+    ///   - animated: true if animations are desired
+    /// - establishes which ViewController is being dismissed
+    /// - makes sure the navigationController list of ViewControlled no longer contains the dismissed viewController
+    /// - calls any closures for the dismissed viewController, if any
+    func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+        
+        guard let dismissedViewController = navigationController.transitionCoordinator?.viewController(forKey: .from) else { return }
+        guard !navigationController.viewControllers.contains(dismissedViewController) else { return }
+        performOnDismissAction(for: dismissedViewController)
+    }
+}
---
+++ Github-Followers/Routers/Router.swift
@@ -0,0 +1,40 @@
+//
+//  Router.swift
+//  Github-Followers
+//
+//  Created by Petre Vane on 19/02/2020.
+//  Copyright © 2020 Petre Vane. All rights reserved.
+//
+
+import UIKit
+
+
+/// Defines methods all concrete routers must implement
+protocol Router: class {
+    
+    /// Presents a viewController
+     /// - Parameters:
+     ///   - viewController: ViewController that should be presented
+     ///   - animated: pass true to animate the transition
+    func present(_ viewController: UIViewController, animated: Bool)
+    
+    
+    /// Presents a viewController
+    /// - Parameters:
+    ///   - viewController: ViewController that should be presented
+    ///   - animated: pass true to animate the transition
+    ///   - onDismiss: closure to be executed when the viewController is dismissed
+    
+    
+    func present(_ viewController: UIViewController, animated: Bool, onDismiss: (() -> Void)?)
+    /// Dismisses a ViewController
+    /// - Parameter animated: pass true to animate the transition
+    func dismiss(animated: Bool)
+}
+
+extension Router {
+    
+    func present(_ viewController: UIViewController, animated: Bool) {
+        present(viewController, animated: animated, onDismiss: nil)
+    }
+}
