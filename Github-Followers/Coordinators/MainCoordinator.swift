ad016df79bda43db819b4904b3183d6b3bdaca0d
Petre Vane <PetreVane@users.noreply.github.com>
2020-02-24 13:53:17 +0100

    Added new NavigationRouter which gets passed to FolloweInfoCoordinator.

--- Github-Followers/Coordinators/MainCoordinator.swift
+++ Github-Followers/Coordinators/MainCoordinator.swift
@@ -12,6 +12,7 @@ import UIKit
 class MainCoordinator: NSObject, Coordinator {
 
     var router: NavigationRouter
+    var presenter = NavigationRouter()
     var childCoordinators = [Coordinator]()
     
     init(navigationRouter: NavigationRouter) {
@@ -25,7 +26,6 @@ class MainCoordinator: NSObject, Coordinator {
         searchCoordinator.parent = self
         searchCoordinator.start()
         childCoordinators.append(searchCoordinator)
-        print("ChildCoordinators contains: \(childCoordinators)")
     }
     
     func startFavoritesCoordinator() {
@@ -34,7 +34,6 @@ class MainCoordinator: NSObject, Coordinator {
         favoritesCoordinator.parent = self
         favoritesCoordinator.start()
         childCoordinators.append(favoritesCoordinator)
-        print("ChildCoordinators contains: \(childCoordinators)")
     }
     
     func startUserListCoordinator(withText text: String) {
@@ -42,14 +41,20 @@ class MainCoordinator: NSObject, Coordinator {
         userListCoordinator.parent = self
         childCoordinators.append(userListCoordinator)
         userListCoordinator.startUserList(withText: text)
-        print("ChildCoordinators contains: \(childCoordinators)")
+    }
+    
+    func startFollowerInfoCoordinator(forFollower follower: Follower) {
+        
+        let followerInfoCoordinator = FollowerInfoCoordinator(navigationRouter: presenter)
+        followerInfoCoordinator.parent = self
+        childCoordinators.append(followerInfoCoordinator)
+        followerInfoCoordinator.presentController(forFollower: follower)
     }
     
     func remove(_ coordinator: Coordinator) {
         for (index, child) in childCoordinators.enumerated() {
             if coordinator === child {
                 childCoordinators.remove(at: index)
-                print("ChildCoordinators contains: \(childCoordinators)")
             }
         }
     }
