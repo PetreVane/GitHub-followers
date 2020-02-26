428e2bb66796232b2d6417a394f69a3f952a9e24
Petre Vane <PetreVane@users.noreply.github.com>
2020-02-26 14:49:46 +0100

    Removed unused properties / commented code

--- Github-Followers/Managers/PersistenceManager.swift
+++ Github-Followers/Managers/PersistenceManager.swift
@@ -18,57 +18,11 @@ struct PersistenceManager {
     
     /// Simple key, to avoid typos
     enum Keys {
-        static let favorites = "favorites"
         static let followers = "SavedFollowers"
     }
     
-    private let storage = UserDefaults.standard
     private let userDocumentsDirectoryPath = FileManager.documentsDirectory
     static let sharedInstance = PersistenceManager()
-    
-
-    /// Gets data from User Defaults
-    ///
-    /// The return value contains a Result type, which holds an array of Followers, in case of success and ErrorManager message in case of failure
-//    func retrieveFavorites() -> [Follower] {
-//        var followers: [Follower] = []
-//
-//        // returns an empty array, in case there is no data saved in User Defaults
-//        guard let retrievedData = storage.object(forKey: Keys.favorites) as? Data else { return followers }
-//        let decoder = JSONDecoder()
-//
-//        do {
-//            let decodedFollowers = try decoder.decode([Follower].self, from: retrievedData)
-//            followers = decodedFollowers
-//            return followers
-//        }
-//        catch {
-//            print(ErrorManager.failedJSONParsing)
-//        }
-//
-//        return followers
-//    }
-    
-    
-    /// Adds data to User Defaults
-    /// - Parameter followers: array of Follower instances
-    /// - return ErrorManager: optional ErrorManager message, in case saving has not completed successfully
-//    func saveFavorite(_ followers: [Follower]) -> ErrorManager? {
-//
-//        var existingFavorites = retrieveFavorites()
-//        existingFavorites.append(contentsOf: followers)
-//        let encoder = JSONEncoder()
-//
-//        do {
-//            let encodedData = try encoder.encode(existingFavorites)
-//            storage.set(encodedData, forKey: Keys.favorites)
-//            return nil
-//        }
-//        catch {
-//            return .failedSavingData
-//        }
-//    }
-    
     
     typealias error = ((ErrorManager?) -> Void)
     /// Updates the content of local plist file
@@ -80,7 +34,7 @@ struct PersistenceManager {
     ///  -  updates the content of the file, based on the selected updateType action
     ///  -  rewrites the file to user's Documents directory
     func updateFavoritesList(with follower: Follower, updateType: PersistenceUpdateType, completion: @escaping error) {
-        print("updateFavoritesList called")
+
         var followers = retrieveSavedFollowers()
         
         switch updateType {
