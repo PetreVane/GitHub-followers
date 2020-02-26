1e6d6a87135304f17d2d769ea01ed7914eed4ddd
Petre Vane <PetreVane@users.noreply.github.com>
2020-02-26 14:51:12 +0100

    Changed constraints; uncommented showMethod

--- Github-Followers/Custom Views/Cells/FavoritesCell.swift
+++ Github-Followers/Custom Views/Cells/FavoritesCell.swift
@@ -13,8 +13,8 @@ class FavoritesCell: UITableViewCell {
     let avatarImageView = AvatarImageView(frame: .zero)
     let userNameLabel = TitleLabel(textAlignment: .left, fontSize: 25)
     static let reuseIdentifier = "FavoritesCell"
-//    let networkManager = NetworkManager.sharedInstance
-//    let cacheManager = CacheManager.sharedInstance
+    let networkManager = NetworkManager.sharedInstance
+    let cacheManager = CacheManager.sharedInstance
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -35,37 +35,48 @@ class FavoritesCell: UITableViewCell {
         
         NSLayoutConstraint.activate([
         
-            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
             avatarImageView.heightAnchor.constraint(equalToConstant: 60),
             avatarImageView.widthAnchor.constraint(equalToConstant: 60),
             
             userNameLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor),
-            userNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: padding * 2),
+            userNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: padding * 4),
             userNameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
             userNameLabel.heightAnchor.constraint(equalToConstant: 40)
         ])
+        
+        layoutIfNeeded()
     }
     
     /// Assigns text & image to cell label & cell imageView
     /// - Parameter follower: follower object for which avatar image and name are shown
-//    func show(_ follower: Follower) {
-//        userNameLabel.text = follower.login
-//        
-//        // checks if the avatarImage already exists in cache
-//        if let cachedImage = cacheManager.retrieveImage(withIdentifier: follower.avatarURL) {
-//            avatarImageView.image = nil
-//            DispatchQueue.main.async { self.avatarImageView.image = cachedImage }; return
-////        } else {
-////            avatarImageView.image = nil
-////            networkManager.fetchAvatars(from: follower.avatarURL) { [weak self] image in
-////
-////                  guard let self = self else { return }
-////                  DispatchQueue.main.async { self.imageView?.image = image }
-////                  self.cacheManager.saveImage(withIdentifier: follower.avatarURL, image: image)
-////              }
-//        }
-//    }
+    func show(_ follower: Follower) {
+        
+        userNameLabel.text = follower.login
+
+        // checks if the avatarImage already exists in cache
+        if let cachedImage = cacheManager.retrieveImage(withIdentifier: follower.avatarURL) {
+            avatarImageView.image = nil
+            DispatchQueue.main.async { self.avatarImageView.image = cachedImage }
+            return
+            
+        } else {
+            
+            networkManager.fetchAvatars(from: follower.avatarURL) { [weak self] image in
+                guard let self = self else { return }
+                DispatchQueue.main.async { self.avatarImageView.image = image }
+                self.cacheManager.saveImage(withIdentifier: follower.avatarURL, image: image)
+            }
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        avatarImageView.image = AvatarImageView.imagePlaceholder
+        userNameLabel.text = nil
+    }
+    
 }
 
 extension FavoritesCell: Comparable {
